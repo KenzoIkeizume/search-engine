be15b2efabbfc6f2d1e47e3c422b337500b572ab
kenzo.ikeizume <kenzo.ikeizume@gamersclub.com.br>
2021-08-21 05:51:05 -0300

    include api security token

---
+++ src/middlewares/auth.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv'
+import jwt from 'jsonwebtoken'
+
+import { response } from '../helpers/http'
+
+dotenv.config()
+
+const { API_AUTH_SECRET, API_AUTH_EXPIRE_IN } = process.env
+
+export const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers.authorization
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if (!token) {
+    return res.status(401).json(response(401, ['unauthorized']))
+  }
+
+  jwt.verify(token, API_AUTH_SECRET, { ignoreExpiration: false }, (err, user) => {
+    if (err) {
+      console.log('Error in generate Token: ', err.name)
+      return res.status(403).json(response(403, ['invalid token']))
+    }
+
+    next()
+  })
+}
+
+export const generateToken = (req, res) => {
+  const token = jwt.sign({ ...req.body }, API_AUTH_SECRET, { expiresIn: API_AUTH_EXPIRE_IN })
+
+  res.status(200).json(response(200, { token }))
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,13 @@ import express from 'express'
 import configs from './configs.js'
 import users from './users.js'
 
+import { generateToken, authenticateToken } from '../middlewares/auth.js'
+
 const router = express.Router()
 
-router.use('/api/configs', configs)
-router.use('/api/users', users)
+router.use('/api/login', generateToken)
+
+router.use('/api/configs', authenticateToken, configs)
+router.use('/api/users', authenticateToken, users)
 
 export default router
